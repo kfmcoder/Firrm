20e6a4dd4379b43e122172486facd4b194ba69b4
Kareem Faraj <kf.profile@gmail.com>
2025-03-28 13:37:29 +0200

    28/03/2025 update

--- firrm.js
+++ firrm.js
@@ -7,10 +7,29 @@ function myFunction(x) {
 }
 //////////////////////////
 
-
 const drawerContainer = document.querySelector('.drawer-container');
 const drawerToggle = document.querySelector('.drawer-toggle');
 
 drawerToggle.addEventListener('click', () => {
     drawerContainer.classList.toggle('open');
 });
+
+
+//// TABS ////
+function openTab(evt, tabName) {
+    // Hide all tab content
+    let tabContents = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove("active");
+    }
+
+    // Remove active class from all buttons
+    let tabButtons = document.getElementsByClassName("tab-button");
+    for (let i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove("active");
+    }
+
+    // Show current tab and activate button
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
